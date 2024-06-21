a57d97bee4f7758f9a04950aac5ccf3f81111c2a
arshiwya <arshiyasohrabi81@gmail.com>
2024-06-22 03:08:14 +0330

    initial home views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
+from products.models import Product, Category
 
-# Create your views here.
+
+class HomePageView(View):
+    http_method_names = ["get"]
+    template_name = 'home/index.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            "categories": Category.objects.all(),
+        }
+        return render(request, template_name=self.template_name, context=context)
