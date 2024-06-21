43db5fb713dfd4fd7fca7d1b22041df8ca0a73a3
arshiwya <arshiyasohrabi81@gmail.com>
2024-06-22 03:15:48 +0330

    login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
 from django.urls import path
+from .views import LoginPageView, LogoutUserView
 
 app_name = 'accounts'
 
 urlpatterns = [
-
+    path('login/', LoginPageView.as_view(), name='login'),
+    path('logout/', LogoutUserView.as_view(), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class LoginPageView(LoginView):
+    template_name = 'accounts/login-page.html'
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+    def form_valid(self, form):
+        auth_login(self.request, form.get_user())
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return '/'
+
+
+class LogoutUserView(View):
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            logout(request)
+            return HttpResponseRedirect(reverse_lazy('home:home'))
+
+        return super().dispatch(request, *args, **kwargs)
