1eb0c25f7cb934eac7721b36699ba86871a44dd0
arshiwya <arshiyasohrabi81@gmail.com>
2024-06-22 21:03:12 +0330

    accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
+
 class User(AbstractUser):
     phone = models.CharField(max_length=20, null=True, blank=True)
     is_admin = models.BooleanField(default=False, verbose_name='Admin status')
@@ -18,6 +19,7 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.get_full_name()
+
 
 class Contact(models.Model):
     GOOD = 1
