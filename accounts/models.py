9330385924b36dc8bc568deff4619d7d5611d708
mubina <mubmubina83@gmail.com>
2024-06-22 19:14:25 +0330

    acc model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.conf import settings
 
 class User(AbstractUser):
     phone = models.CharField(max_length=20, null=True, blank=True)
@@ -18,3 +18,19 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.get_full_name()
+
+class Contact(models.Model):
+    GOOD = 1
+    EXCELLENT = 2
+    BAD = 3
+
+    STATUS_CHOICES = (
+        (GOOD, 'Good'),
+        (EXCELLENT, 'Excellent'),
+        (BAD, 'Bad')
+    )
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    message = models.TextField()
+    status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=GOOD)
