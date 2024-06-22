a4c1f518de3cbbd196bb833ff7bf2dfad2e791f9
arshiwya <arshiyasohrabi81@gmail.com>
2024-06-22 14:48:40 +0330

    storage views

--- storage/urls.py
+++ storage/urls.py
@@ -1,8 +1,9 @@
-from django.contrib import admin
 from django.urls import path
+from .views import MaterialHomePageView, UpdateMaterialView
 
 app_name = 'storage'
 
 urlpatterns = [
-
+    path('', MaterialHomePageView.as_view(), name="materials"),
+    path('update/<int:pk>/', UpdateMaterialView.as_view(), name="update-material"),
 ]
--- storage/views.py
+++ storage/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, UpdateView
+from .models import Material
 
-# Create your views here.
+
+class MaterialHomePageView(ListView):
+    template_name = 'storage/index.html'
+    queryset = Material.objects.all()
+    context_object_name = "materials"
+
+
+class UpdateMaterialView(UpdateView):
+    model = Material
+    fields = ["name", "amount"]
+    template_name = "storage/material-update.html"
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial["amount"] = self.object.amount
+        return initial
+
+    def get_success_url(self):
+        pk = self.object.id
+        return f"/storage/update/{pk}"
