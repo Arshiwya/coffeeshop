ec9b0fc88f99145382eb4f11ae83971bc78a1e0e
arshiwya <arshiyasohrabi81@gmail.com>
2024-06-23 15:39:08 +0330

    Products-views-complete

--- coffeeshop/urls.py
+++ coffeeshop/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('users/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
     path('storage/', include('storage.urls')),
+    path('products/', include('products.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DeleteView
+from .models import Product, Category
 
-# Create your views here.
+
+class CreateProductView(CreateView):
+    model = Product
+    template_name = 'products/create-product.html'
+    fields = ['name', "sugar",
+              "coffee",
+              "flour",
+              "chocolate",
+              "categories", ]
+
+    success_url = '/'
+
+
+class UpdateProductView(UpdateView):
+    queryset = Product.objects.all()
+    template_name = 'products/update-product.html'
+    fields = ['name', "sugar",
+              "coffee",
+              "flour",
+              "chocolate",
+              "categories", ]
+
+    def get_success_url(self):
+        url = f'/products/update/{self.object.id}'
+        return url
+
+
+class DeleteProductView(DeleteView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'products/delete-product.html'
+    success_url = '/'
+
+
+class CreateCategoryView(CreateView):
+    model = Category
+    template_name = 'products/create-category.html'
+    fields = ['name', ]
+
+    def get_success_url(self):
+        print(self.object.id)
+
+
+class UpdateCategoryView(UpdateView):
+    queryset = Category.objects.all()
+    template_name = 'products/update-category.html'
+    fields = ['name', ]
+
+    def get_success_url(self):
+        url = f'/products/category/update/{self.object.id}'
+        return url
+
+
+class DeleteCategoryView(DeleteView):
+    model = Category
+    context_object_name = 'category'
+    template_name = 'products/delete-category.html'
+    success_url = '/'
