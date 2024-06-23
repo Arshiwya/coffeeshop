48f3a992d5bd71a279afdb7958d7819e29f99647
arshiwya <arshiyasohrabi81@gmail.com>
2024-06-23 12:24:32 +0330

    SignIn User

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LoginPageView, LogoutUserView
+from .views import SignUserView, LoginPageView, LogoutUserView
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('signin/', SignUserView.as_view(), name='signin'),
     path('login/', LoginPageView.as_view(), name='login'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,49 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from .models import User
+from .forms import SignUserForm
+from .mixins import AnonymousUserMixin
+
+
+class SignUserView(AnonymousUserMixin, CreateView):
+    model = User
+    template_name = 'accounts/signin-page.html'
+    form_class = SignUserForm
+
+    def form_valid(self, form):
+
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password"]
+        password2 = form.cleaned_data["password2"]
+        email = form.cleaned_data["email"]
+
+        if password == password2:
+            try:
+                validate_password(password=password)
+                user = User.objects.create_user(username=username, password=password, email=email)
+                auth_login(self.request, user)
+                return HttpResponseRedirect(self.get_success_url())
+
+            except ValidationError as error:
+                for e in error:
+                    form.add_error(field='password', error=e)
+
+                return self.form_invalid(form)
+
+        else:
+            form.add_error(field="password", error='Passwords are not match .')
+            return self.form_invalid(form)
+
+    def get_success_url(self):
+        return '/'
 
 
 class LoginPageView(LoginView):
