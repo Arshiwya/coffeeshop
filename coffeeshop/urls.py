97bf7bb3862b3efd7ad0768d68aca979dff9278c
arshiwya <arshiyasohrabi81@gmail.com>
2024-06-25 02:33:50 +0330

    products list and edtails created

--- coffeeshop/urls.py
+++ coffeeshop/urls.py
@@ -3,12 +3,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('users/', include('accounts.urls')),
-    path('orders/', include('orders.urls')),
-    path('storage/', include('storage.urls')),
-    path('products/', include('products.urls')),
-] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('home.urls')),
+                  path('users/', include('accounts.urls')),
+                  path('orders/', include('orders.urls')),
+                  path('storage/', include('storage.urls')),
+                  path('products/', include('products.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
